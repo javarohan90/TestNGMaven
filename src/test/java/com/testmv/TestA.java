398be7f2b1f3b73e58aacfa9c47408ed66547128
Rohan Ghatage <rohan.ghatage@ecotechservices.com>
2020-12-17 16:59:10 +0530

    TestNG Maven Integration

---
+++ src/test/java/com/testmv/TestA.java
@@ -0,0 +1,13 @@
+package com.testmv;
+
+import org.testng.annotations.Test;
+
+public class TestA {
+	
+	@Test
+	public void MA()
+	{
+		System.out.println("MA Run");
+	}
+
+}
---
+++ src/test/java/com/testmv/TestB.java
@@ -0,0 +1,13 @@
+package com.testmv;
+
+import org.testng.annotations.Test;
+
+public class TestB {
+	
+	@Test
+	public void MB()
+	{
+		System.out.println("MB Run");
+	}
+
+}
